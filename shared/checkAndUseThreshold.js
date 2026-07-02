f5db6911fed910067aa83dbe67090ba5531286e6
JohnTech <122186344+JohnTechAU@users.noreply.github.com>
2026-07-02 22:54:52 +1000

    Fix checkandUseThreshold

--- shared/checkAndUseThreshold.js
+++ shared/checkAndUseThreshold.js
@@ -1,9 +1,7 @@
 // Checks if the current value is below the threshold and uses the skill if not on cooldown
 function checkAndUseThreshold(currentValue, maxValue, thresholdBelowMax, skillName) {
 	if (currentValue <= maxValue - thresholdBelowMax) {
-		if (is_on_cooldown(skillName)) 
-            set_message(skillName + " is on cooldown");
-            return false;
+		if (is_on_cooldown(skillName)) return false;
 		use_skill(skillName);
 	}
 }
